4226ae41736b4a42bcd9eaf918a671285b025e97
AmirH <AmirH.KHALI@gmail.com>
2023-01-20 21:56:23 +0330

    implement jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,14 @@ public class PlayerController : MonoBehaviour
     private Vector2 velocity;
     private float inputAxis;
 
-    public float speed = 10f;
+    public float speed = 8f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2f);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
 
     private void Awake()
     {
@@ -16,13 +23,16 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Move();
-    }
+        HorizontalMovement();
 
-    private void Move()
-    {
-        inputAxis = Input.GetAxis("Horizontal");
-        velocity.x = Mathf.MoveTowards(velocity.x, speed * inputAxis, speed * Time.deltaTime);
+        grounded = _rigidbody.Raycast(Vector2.down);
+
+        if (grounded)
+        {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void FixedUpdate()
@@ -31,5 +41,64 @@ public class PlayerController : MonoBehaviour
         position += velocity * Time.deltaTime;
 
         _rigidbody.MovePosition(position);
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, speed * inputAxis, speed * Time.deltaTime);
+
+        // check if running into a wall
+        if (_rigidbody.Raycast(Vector2.right * velocity.x))
+        {
+            velocity.x = 0f;
+        }
+
+        // flip sprite to face direction
+        if (velocity.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (velocity.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+    }
+
+    private void GroundedMovement()
+    {
+        // prevent gravity from infinitly building up
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        // perform jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        // check if falling
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        // apply gravity and terminal velocity
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            // stop vertical movement if mario bonks his head
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
+                velocity.y = 0f;
+            }
+        }
     }
 }
